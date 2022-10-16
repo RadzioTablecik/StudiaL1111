cb3132b268194a63d8fbe0fdc64fdf7759113b8d
dupa <kpiech70@gmail.com>
2022-10-17 01:15:23 +0200

    toString UT

--- src/test/java/org/example/TreeToStringTest.java
+++ src/test/java/org/example/TreeToStringTest.java
@@ -6,5 +6,10 @@ public class TreeToStringTest {
 
     @org.junit.Test
     public void testToString() {
+        Tree<Integer> treeInt = new Tree<>();
+        treeInt.insert(5);
+        treeInt.insert(3);
+        String result = treeInt.toString();
+        assertEquals("(5:(3:():()):())",result);
     }
 }
